2b7baaf1d69721d41c1a1c7758893b14b9c2659e
timeless perfections <109487597+timeless-perfections@users.noreply.github.com>
2022-07-20 18:50:38 +0300

    Add files via upload

---
+++ massBodyİndex.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class massBodyİndex {
+    public static void main(String[] args) {
+        double weight,size,index;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Write Your Height : ");
+        size = input.nextDouble();
+
+        System.out.println("Type Your Weight");
+        weight =input.nextDouble();
+
+        index = weight/(size * size);
+        System.out.println("Your Body Mass Index : "+ index);
+
+        if (index < 1.8){
+            System.out.print("You Are Weak");
+        }
+
+
+    }
+
+}
