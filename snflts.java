20a7679aed8ee5b93016ca40ca8a3faedcab4718
timeless perfections <109487597+timeless-perfections@users.noreply.github.com>
2022-07-22 16:00:48 +0300

    Add files via upload

---
+++ snflts.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+//snflts==Sorting Numbers from Largest to Smaller;
+
+
+public class snflts {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner input = new Scanner(System.in);
+        System.out.println("First Number : ");
+        a = input.nextInt();
+
+        System.out.println("Second Number : ");
+        b = input.nextInt();
+
+
+        System.out.println("Third Number : ");
+        c = input.nextInt();
+
+        if ((a < b && a < c)) {
+            if (b < c) {
+                System.out.println("In order from smallest to largest : " + a + ", " + b + ", " + c + ".");
+            } else {
+                System.out.println("In order from smallest to largest : " + a + ", " + c + ", " + b + ".");
+            }
+        } else if ((b < a && b < c)) {
+            if (a < c) {
+                System.out.println("In order from smallest to largest : " + b + ", " + a + ", " + c + ".");
+            } else {
+                System.out.println("In order from smallest to largest : " + b + ", " + c + ", " + a + ".");
+            }
+        } else {
+            if (a < b) {
+                System.out.println("In order from smallest to largest : " + c + ", " + a + ", " + b + ".");
+            } else {
+                System.out.println("In order from smallest to largest : " + c + ", " + b + ", " + a + ".");
+            }
+
+
+        }
+
+    }
+}
